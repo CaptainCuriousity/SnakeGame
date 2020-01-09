e961e975d394974ae1e7fa1957540fc32db2e9ff
YanDushkin <42182463+YanDushkin@users.noreply.github.com>
2020-01-09 23:57:47 +0700

    Второй коммит. Сделано стартовое окно.

--- code.py
+++ code.py
@@ -1,4 +1,6 @@
 import pygame
+import os
+import sys
 
 
 class Board:
@@ -30,6 +32,7 @@ class Snake(Board):
         super().__init__(width, height)
 
         # Игровое поле. 0 соответствует пустой клетке, 1 - барьеру, 2 - еде
+        # 3 - голове, 4 - части туловища
         # В случае, если уже есть игровое поле и его размеры совпадают с размером доски,
         # То ставится оно, в противном случае генерируется новое
         if field is not None and len(field) == height and len(field[0]) == width:
@@ -51,28 +54,71 @@ class Snake(Board):
                 elif self.field[j][i] == 2:
                     pygame.draw.rect(screen, (255, 255, 255), rect, 1)
                     pygame.draw.circle(
-                        screen,
-                        (255, 255, 0),
-                        (
-                            self.left + i * self.cell_size + self.cell_size // 2,
-                            self.top + j * self.cell_size + self.cell_size // 2
-                        ),
+                        screen, (255, 255, 0),
+                        (self.left + i * self.cell_size + self.cell_size // 2,
+                         self.top + j * self.cell_size + self.cell_size // 2),
                         self.cell_size // 2 - 2
                     )
 
 
-pygame.init()
-SIZE = (500, 500)
-screen = pygame.display.set_mode(SIZE)
+def load_image(name, color_key=None):
+    fullname = os.path.join("data", name)
+    image = pygame.image.load(fullname).convert()
 
-field = [
-    [0, 0, 0, 0, 1],
-    [0, 0, 1, 1, 1],
-    [0, 0, 1, 0, 0],
-    [0, 0, 1, 1, 1],
-    [2, 0, 0, 0, 1]
-]
-board = Snake(5, 5, field)
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗМЕЙКА", "На торе"]
+    background = pygame.transform.scale(load_image('background.png'), (WIDTH, HEIGHT))
+    screen.blit(background, (0, 0))
+
+    font = pygame.font.Font("joystix monospace.ttf", 70)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 50
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+
+
+def end_screen():
+    pass
+
+
+def draw_score():
+    pass
+
+
+pygame.init()
+WIDTH, HEIGHT = SIZE = (500, 500)
+screen = pygame.display.set_mode(SIZE)
+start_screen()
+
+board = Snake(10, 10)
+board.set_view(50, 50, 40)
 running = True
 while running:
     for event in pygame.event.get():
